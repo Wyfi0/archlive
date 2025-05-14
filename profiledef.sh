6767bd8e30967ecf122f3d3d41f3f5f25cd91f6f
Wyfi0 <wyfi08g@proton.me>
2025-05-14 14:59:48 -0400

    initial commit 2

---
+++ profiledef.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# shellcheck disable=SC2034
+
+iso_name="raina's_arch"
+iso_label="RAINA_ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
+iso_publisher="Raina <https://github.com/Wyfi0>"
+iso_application="Arch Linux with my modifications"
+iso_version="$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y.%m.%d)"
+install_dir="arch"
+buildmodes=('iso')
+bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
+           'uefi-ia32.grub.esp' 'uefi-x64.grub.esp'
+           'uefi-ia32.grub.eltorito' 'uefi-x64.grub.eltorito')
+arch="x86_64"
+pacman_conf="pacman.conf"
+airootfs_image_type="ext4+squashfs"
+airootfs_image_tool_options=('-Xhc')
+bootstrap_tarball_compression=(zstd -c -T0 --long -19)
+file_permissions=(
+  ["/etc/shadow"]="0:0:400"
+)
