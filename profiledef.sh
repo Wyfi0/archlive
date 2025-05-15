bf833153ce953db1bbd47f41c4185149d0a03a94
Wyfi0 <wyfi08g@proton.me>
2025-05-15 00:20:06 -0400

    changed compression options and changed some file permissions

--- profiledef.sh
+++ profiledef.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2034
 
-iso_name="raina's_arch"
+iso_name="rainas_arch"
 iso_label="RAINA_ARCH_$(date --date="@${SOURCE_DATE_EPOCH:-$(date +%s)}" +%Y%m)"
 iso_publisher="Raina <https://github.com/Wyfi0>"
 iso_application="Arch Linux with my modifications"
@@ -14,8 +14,11 @@ bootmodes=('bios.syslinux.mbr' 'bios.syslinux.eltorito'
 arch="x86_64"
 pacman_conf="pacman.conf"
 airootfs_image_type="ext4+squashfs"
-airootfs_image_tool_options=('-Xhc')
+airootfs_image_tool_options=('-comp' 'xz' '-Xbcj' 'x86' '-b' '1M' '-Xdict-size' '1M')
 bootstrap_tarball_compression=(zstd -c -T0 --long -19)
 file_permissions=(
   ["/etc/shadow"]="0:0:400"
+  ["/etc/gshadow"]="0:0:400"
+  ["/etc/sudoers"]="0:0:400"
+  ["/etc/sudoers.d/"]="0:0:400"
 )
